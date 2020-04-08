04dfccf749f9fc8115240393ea3f2b6b2eab50a8
alexwong <11878166+alexwong@users.noreply.github.com>
2020-04-08 14:30:33 -0700

    Add a todo

--- python/tvm/relay/op/contrib/tidl.py
+++ python/tvm/relay/op/contrib/tidl.py
@@ -405,7 +405,7 @@ def _split_whitelist_fn(attrs, args):
     supported = True
     return supported
 
-
+#TODO: Remove these ops after composite functions work with mergecompilerregions
 @reg.register("squeeze", "target.tidl")
 def _softmax_whitelist_fn(attrs, args):
     supported = (attrs.axis != 2)
