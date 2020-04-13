5f8c3f307fc34e49e7f52d6e4dd2d26de9c7e073
alexwong <11878166+alexwong@users.noreply.github.com>
2020-04-13 12:19:29 -0700

    Add yolo test:

--- tests/python/relay/test_tidl_annotation.py
+++ tests/python/relay/test_tidl_annotation.py
@@ -24,6 +24,36 @@ from tvm import relay
 from tvm.relay import transform
 import tvm.relay.op.contrib.tidl
 
+# For darknet tests
+#Using darknet...
+import sys
+from tvm.contrib.download import download_testdata
+download_testdata.__test__ = False
+from tvm.relay.testing.darknet import LAYERTYPE
+from tvm.relay.testing.darknet import __darknetffi__
+from tvm.relay.frontend.darknet import ACTIVATION
+
+REPO_URL = 'https://github.com/dmlc/web-data/blob/master/darknet/'
+
+DARKNET_LIB = 'libdarknet_mac2.0.so'
+DARKNETLIB_URL = REPO_URL + 'lib_osx/' + DARKNET_LIB + '?raw=true'
+
+if sys.platform in ['linux', 'linux2']:
+    DARKNET_LIB = 'libdarknet2.0.so'
+    DARKNET_URL = REPO_URL + 'lib/' + DARKNET_LIB + '?raw=true'
+elif sys.platform == 'darwin':
+    DARKNET_LIB = 'libdarknet_mac2.0.so'
+    DARKNET_URL = REPO_URL + 'lib_osx/' + DARKNET_LIB + '?raw=true'
+else:
+    err = "Darknet lib is not supported on {} platform".format(sys.platform)
+    raise NotImplementedError(err)
+
+LIB = __darknetffi__.dlopen(download_testdata(DARKNETLIB_URL, DARKNET_LIB, module='darknet'))
+
+DARKNET_TEST_IMAGE_NAME = 'dog.jpg'
+DARKNET_TEST_IMAGE_URL = REPO_URL + 'data/' + DARKNET_TEST_IMAGE_NAME +'?raw=true'
+DARKNET_TEST_IMAGE_PATH = download_testdata(DARKNET_TEST_IMAGE_URL, DARKNET_TEST_IMAGE_NAME, module='data')
+
 def test_tidl_annotation():
 
     dtype = "float32"
@@ -72,7 +102,6 @@ def convert_to_list(x):
     return x
 
 def test_tidl_mobilenet():
-
     import tensorflow as tf
     import tvm.relay.testing.tf as tf_testing
 
@@ -109,6 +138,112 @@ def test_tidl_mobilenet():
             print("---------- Partioned Graph ----------")
             mod4 = transform.PartitionGraph()(mod4)
             print(mod4.astext(show_meta_data=False))
+
+def _read_memory_buffer(shape, data, dtype='float32'):
+    length = 1
+    for x in shape:
+        length *= x
+    data_np = np.zeros(length, dtype=dtype)
+    for i in range(length):
+        data_np[i] = data[i]
+    return data_np.reshape(shape)
+
+def _load_net(cfg_url, cfg_name, weights_url, weights_name):
+    cfg_path = download_testdata(cfg_url, cfg_name, module='darknet')
+    weights_path = download_testdata(weights_url, weights_name, module='darknet')
+    net = LIB.load_network(cfg_path.encode('utf-8'), weights_path.encode('utf-8'), 0)
+    return net
+
+def verify_darknet_frontend(net, build_dtype='float32'):
+    '''Test network with given input image on both darknet and tvm'''
+    def get_darknet_output(net, img):
+        LIB.network_predict_image(net, img)
+        out = []
+        for i in range(net.n):
+            layer = net.layers[i]
+            if layer.type == LAYERTYPE.REGION:
+                attributes = np.array([layer.n, layer.out_c, layer.out_h,
+                                       layer.out_w, layer.classes,
+                                       layer.coords, layer.background],
+                                      dtype=np.int32)
+                out.insert(0, attributes)
+                out.insert(0, _read_memory_buffer((layer.n*2, ), layer.biases))
+                layer_outshape = (layer.batch, layer.out_c,
+                                  layer.out_h, layer.out_w)
+                out.insert(0, _read_memory_buffer(layer_outshape, layer.output))
+            elif layer.type == LAYERTYPE.YOLO:
+                attributes = np.array([layer.n, layer.out_c, layer.out_h,
+                                       layer.out_w, layer.classes,
+                                       layer.total],
+                                      dtype=np.int32)
+                out.insert(0, attributes)
+                out.insert(0, _read_memory_buffer((layer.total*2, ), layer.biases))
+                out.insert(0, _read_memory_buffer((layer.n, ), layer.mask, dtype='int32'))
+                layer_outshape = (layer.batch, layer.out_c,
+                                  layer.out_h, layer.out_w)
+                out.insert(0, _read_memory_buffer(layer_outshape, layer.output))
+            elif i == net.n-1:
+                if layer.type == LAYERTYPE.CONNECTED:
+                    darknet_outshape = (layer.batch, layer.out_c)
+                elif layer.type in [LAYERTYPE.SOFTMAX]:
+                    darknet_outshape = (layer.batch, layer.outputs)
+                else:
+                    darknet_outshape = (layer.batch, layer.out_c,
+                                        layer.out_h, layer.out_w)
+                out.insert(0, _read_memory_buffer(darknet_outshape, layer.output))
+        return out
+
+    dtype = 'float32'
+
+    img = LIB.letterbox_image(LIB.load_image_color(DARKNET_TEST_IMAGE_PATH.encode('utf-8'), 0, 0), net.w, net.h)
+    darknet_output = get_darknet_output(net, img)
+    batch_size = 1
+    data = np.empty([batch_size, img.c, img.h, img.w], dtype)
+    i = 0
+    for c in range(img.c):
+        for h in range(img.h):
+            for k in range(img.w):
+                data[0][c][h][k] = img.data[i]
+                i = i + 1
+
+    (mod, params) = _get_tvm_output(net, data, build_dtype)
+    return (mod, params)
+
+def _get_tvm_output(net, data, build_dtype='float32', states=None):
+    '''Compute TVM output'''
+    dtype = 'float32'
+    mod, params = relay.frontend.from_darknet(net, data.shape, dtype)
+    return (mod, params)
+
+def test_tidl_yolo():
+    model_name = 'yolov3'
+    cfg_name = model_name + '.cfg'
+    weights_name = model_name + '.weights'
+    cfg_url = 'https://github.com/pjreddie/darknet/blob/master/cfg/' + cfg_name + '?raw=true'
+    weights_url = 'http://pjreddie.com/media/files/' + weights_name + '?raw=true'
+    net = _load_net(cfg_url, cfg_name, weights_url, weights_name)
+    build_dtype = {}
+    front_out = verify_darknet_frontend(net, build_dtype)
+
+    mod = front_out[0]
+    LIB.free_network(net)
+
+    print('---------- Original Graph ----------')
+    mod = relay.transform.RemoveUnusedFunctions()(mod)
+    print(mod.astext(show_meta_data=False))
+    print('---------- Merge Composite Functions ----------')
+    mod = tvm.relay.op.contrib.tidl._merge_sequential_ops(mod) #Merge sequence of ops into composite functions/ops
+    print(mod.astext(show_meta_data=False))
+    print("---------- Annotated Graph ----------")
+    mod = transform.AnnotateTarget("tidl")(mod) #Looks at annotated ops and marks them in the graph with compiler.begin and compiler.end
+    print(mod.astext(show_meta_data=False))
+    print("---------- Merge Compiler Regions ----------")
+    mod = transform.MergeCompilerRegions()(mod) #Merge annotated regions together that use the same external target, combines marked regions for each target
+    print(mod.astext(show_meta_data=False))
+    print("---------- Partioned Graph ----------")
+    mod = transform.PartitionGraph()(mod)
+    print(mod.astext(show_meta_data=False))
+
 
 def test_tidl_mobilenet_no_composite():
 
@@ -147,6 +282,7 @@ def test_tidl_mobilenet_no_composite():
             print(mod4.astext(show_meta_data=False))
 
 if __name__ == '__main__':
-    test_tidl_annotation()
-    test_tidl_mobilenet()
+    #test_tidl_annotation()
+    #test_tidl_mobilenet()
     #test_tidl_mobilenet_no_composite()
+    test_tidl_yolo()
